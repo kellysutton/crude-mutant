ccc417e9a5e8f630a05a21b06893364998372467
Kelly Sutton <kelly.sutton@gusto.com>
2018-12-26 14:38:22 -0800

    Implementing FileWriter functionality

--- lib/crude_mutant/file_writer.rb
+++ lib/crude_mutant/file_writer.rb
@@ -3,7 +3,11 @@
 module CrudeMutant
   class FileWriter
     def self.write(file_path, contents_as_array)
-
+      File.open(file_path, 'w') do |f|
+        contents_as_array.each do |line|
+          f.write(line)
+        end
+      end
     end
   end
 end
