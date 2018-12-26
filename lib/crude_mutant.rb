a67be20f141880a57d06eb81c46cd5eb84877361
Kelly Sutton <kelly.sutton@gusto.com>
2018-12-26 15:59:50 -0800

    Progress bar

--- lib/crude_mutant.rb
+++ lib/crude_mutant.rb
@@ -16,6 +16,8 @@ module CrudeMutant
   end
 
   class Progress
+    attr_reader :run_result, :total_runs_to_perform
+
     def initialize(total_runs_to_perform, run_result)
       @total_runs_to_perform = total_runs_to_perform
       @run_result = run_result
