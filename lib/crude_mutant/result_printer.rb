bb6091220994f37c7384f079fabf0fcd07dabad1
Kelly Sutton <michael.k.sutton@gmail.com>
2018-12-26 19:52:55 -0800

    Add some whitespace

--- lib/crude_mutant/result_printer.rb
+++ lib/crude_mutant/result_printer.rb
@@ -28,6 +28,7 @@ module CrudeMutant
           stream.print "#{line}"
         end
 
+        stream.print "\n"
         stream.print "Finished mutating #{result.file_path}. ^^^ Results above ^^^\n"
         stream.print "Performed #{result.run_results.size} line mutations in total.\n"
         stream.print "There are #{red(result.successful_runs_even_with_mutations.size)} #{red('problematic lines')}:\n"
