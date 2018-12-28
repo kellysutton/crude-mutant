32f7fd824fa49b39ee25e9ee76116fd142321dcb
Kelly Sutton <michael.k.sutton@gmail.com>
2018-12-27 17:30:21 -0800

    Adding a spec for Progress#avg_time

---
+++ spec/crude_mutant/progress_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+RSpec.describe CrudeMutant::Progress do
+  describe '#avg_time' do
+    subject { described_class.new(total_runs_to_perform, run_results).avg_time }
+    let(:total_runs_to_perform) { double }
+    let(:run_results) do
+      [
+        instance_double(CrudeMutant::RunResult, time_taken: 2.0),
+        instance_double(CrudeMutant::RunResult, time_taken: 3.0)
+      ]
+    end
+
+    it { is_expected.to eq(2.5) }
+  end
+end
